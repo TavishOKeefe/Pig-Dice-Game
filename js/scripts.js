42af188cf533df1870a0184735750feb6b331c12
Devin Sweeting and Tavish OKeefe <tavish@>
2018-10-31 11:50:38 -0700

    add logic to recognize 1

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,24 @@ function Players (user, total, turn) {
   this.turn = 0
 }
 
-Players.prototype.rollDi = function (){
+Players.prototype.rollDi = function() {
   var roll = Math.floor(Math.random()*(7-1) + 1);
+  if (roll !== 1){
   this.turn = roll + this.turn;
   console.log(this.turn);
-  }
+} else if (roll === 1){
+  this.total = this.turn - this.turn;
+  console.log(this.total)
+}
+}
 
-Players.prototype.addScore = function (){
+Players.prototype.addScore = function() {
   this.total = this.turn + this.total;
   console.log(this.total);
 }
+
+// Players.prototype.endTurn = function() {
+//   if (this.turn - (this.turn + roll) === 1) {
 
 
 playerOne.rollDi();
